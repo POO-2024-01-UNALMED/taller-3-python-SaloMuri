e07767577cdedd57687e8c1da14c5b1b30b0e829
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-02-25 15:11:41 -0500

    Segundo
    Se agregó la clase TV.

--- televisores.py
+++ televisores.py
@@ -7,3 +7,43 @@ class Marca:
 
     def getNombre(self):
         return self._nombre
+    
+class TV:
+    numTV = 0
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._precio = 500
+        self._volumen = 1
+        self._control = None
+
+    def setMarca(self, marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+    
+    def setCanal(self, canal):
+        self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+    
+    def setPrecio(self, precio):
+        self._precio = precio
+    
+    def getPrecio(self):
+        return self._precio
+    
+    def setVolumen(self, volumen):
+        self._volumen = volumen
+
+    def getVolumen(self):
+        return self._volumen
+    
+    def setControl(self, control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
